6b46500a1a215ccce9e1d66071fcb62195a23fad
Dolat Hamza <dolathamza8019@gmail.com>
2021-12-29 11:09:56 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from 'react-router-dom';
+import {
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBContainer,
+  MDBIcon } from 'mdb-react-ui-kit';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import FormPage from './components/form';
+class App extends Component {
+  render() {
+    return (
+      
+    <Router>
+      
+     <div className="App">
+	<ul>
+	<li>
+		<Link to="/">Home</Link>
+	</li>
+	<li>
+		<Link to="/about">About Us</Link>
+	</li>
+	<li>
+		<Link to="/contact">Contact Us</Link>
+	</li>
+  <li>
+		<Link to="/form">Form Page</Link>
+	</li>
+	</ul>
+  <Routes>
+                 <Route exact path='/' element={< Home />}></Route>
+                 <Route exact path='/about' element={< About />}></Route>
+                 <Route exact path='/contact' element={< Contact />}></Route>
+                 <Route exact path='/form' element={< FormPage />}></Route>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          </Routes>
+</div>
+
+    </Router>
   );
-}
+  }
+  }
+  
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Home (){
+	return <h1>Hello There!
+	</h1>
+}
+
+export default Home;
